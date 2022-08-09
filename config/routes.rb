9a608f404478503e8674edfa34665c0b215bf338
LiRezende <librusamolin@gmail.com>
2022-08-09 10:05:21 -0300

    Home controller created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'home/index'
+  end
   resources :idioms
   devise_for :users
+  get 'inicio', to: 'site/home#index'
+  root to: 'site/home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
