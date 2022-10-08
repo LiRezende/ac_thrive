68c64dfcbf7b2fd772b4598990073d25a0fc55d6
LiRezende <librusamolin@gmail.com>
2022-10-08 18:58:40 -0300

    Roles configured in user/sign_up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :status
+  has_many :roles
   has_one :person
   
   accepts_nested_attributes_for :status
+  accepts_nested_attributes_for :roles
   accepts_nested_attributes_for :person
 
   def with_person
