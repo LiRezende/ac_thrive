d9ff0e420099a9831878e5bbbc5483c524526440
LiRezende <librusamolin@gmail.com>
2022-11-27 13:50:28 -0300

    RG mask conigured.

--- public/assets/javascripts/dashboard-index.js
+++ public/assets/javascripts/dashboard-index.js
@@ -1,36 +1,48 @@
-const sideMenu = document.querySelector('aside');
-const sideMenuItem = document.querySelectorAll('.sidebar a');
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn = document.querySelector('#close-btn');
-const themeToggler = document.querySelector('.theme-toggler');
-const input = document.querySelector('#cpf')
+const sideMenu = document.querySelector('aside')
+const sideMenuItem = document.querySelectorAll('.sidebar a')
+const menuBtn = document.querySelector('#menu-btn')
+const closeBtn = document.querySelector('#close-btn')
+const themeToggler = document.querySelector('.theme-toggler')
+const inputIdentity = document.querySelector('#identity')
+const inputCpf = document.querySelector('#cpf')
 
 // Open and Close Mobile Menu
 menuBtn.addEventListener('click', () => {
-    sideMenu.style.display == "block" ? sideMenu.style.display = "none" : sideMenu.style.display = "block";
+    sideMenu.style.display == "block" ? sideMenu.style.display = "none" : sideMenu.style.display = "block"
 })
 
 // Apply Dark Theme
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
-    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
 })
 
 sideMenuItem.forEach(item => {
     item.addEventListener('click', function () {
-        sideMenuItem.forEach(btn => btn.classList.remove('active'));
+        sideMenuItem.forEach(btn => btn.classList.remove('active'))
         this.classList.add('active');        
-    });
-});
+    })
+})
+
+// RG Mask
+inputIdentity.addEventListener('keypress', () => {
+    let inputLength = inputIdentity.value.length
+
+    if (inputLength == 2 || inputLength == 7) {
+        inputIdentity.value += '.'
+    } else if (inputLength == 11) {
+        inputIdentity.value += '-'
+    }
+})
 
 // CPF Mask
-input.addEventListener('keypress', () => {
-    let inputLength = input.value.length
+inputCpf.addEventListener('keypress', () => {
+    let inputLength = inputCpf.value.length
 
     if (inputLength == 3 || inputLength == 7) {
-        input.value += '.'
+        inputCpf.value += '.'
     } else if (inputLength == 11) {
-        input.value += '-'
+        inputCpf.value += '-'
     }
 })
