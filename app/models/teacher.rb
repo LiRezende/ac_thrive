e878048d857bede3a94f655299ae0fcefa072bda
LiRezende <librusamolin@gmail.com>
2022-12-13 07:30:18 -0300

    Fixing validations in fee field.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,4 +3,7 @@ class Teacher < ApplicationRecord
   belongs_to :idiom
   has_many :schedules
   has_one :student
+
+  validates :fee, numericality: { greater_than_or_equal_to: 0, less_than: BigDecimal(10**3) },
+                     format: { with: /\A\d{1,3}(\.\d{1,2})?\z/ }
 end
--- db/migrate/20221009150528_create_teachers.rb
+++ db/migrate/20221009150528_create_teachers.rb
@@ -1,7 +1,7 @@
 class CreateTeachers < ActiveRecord::Migration[5.2]
   def change
     create_table :teachers, id: :uuid do |t|
-      t.decimal :fee
+      t.decimal :fee, precision: 5, scale: 2
       t.references :person, foreign_key: true, type: :uuid
       t.references :idiom, foreign_key: true, type: :uuid
 
